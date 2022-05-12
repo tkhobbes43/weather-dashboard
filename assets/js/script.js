ff5312d8f09a0eba0100ea2ca3ce9f85e97e8864
Tommy Lee <t.k.hobbes43@gmail.com>
2022-05-11 22:37:04 -0500

    fetching from API is console.log response finally

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,23 +3,24 @@ var cityInput = document.getElementById('input-box');
 
 var cityFormSubmit = function (event) {
     event.preventDefault();
-    var city = cityInput.val();
+    var city = cityInput.value;
     if (city) {
-        currentWeather(city);
-        weatherContainerEl.textContent = '';
+        currentWeather();
         cityInput.value = '';
     } else {
         alert('Please enter a real City name');
     }
 };  
 
-function currentWeather(city) {
+function currentWeather() {
+    var city = cityInput.value;
+    console.log(city);
     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=d23b9ff5efa45588d81ffe68aaf47963";
     fetch(queryURL)
         .then(function (response) {
+            console.log(response);
             return response.json();
         })
-    console.log("hello");
 }
 
 fetchButton.addEventListener('click', currentWeather);
