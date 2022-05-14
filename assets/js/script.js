6b9f426b173037c90afa9b161162a9910fe4c03b
Tommy Lee <t.k.hobbes43@gmail.com>
2022-05-13 19:56:57 -0500

    updated script to fetch onecall api from open weather api for five day forecast and for uv index data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,36 @@
 var fetchButton = document.getElementById('fetch-button');
 var cityInput = document.getElementById('input-box');
 
+// how to search for weather by the city
 var cityFormSubmit = function (event) {
     event.preventDefault();
     var city = cityInput.value;
     if (city) {
         currentWeather();
         cityInput.value = '';
-    } else {
-        alert('Please enter a real City name');
     }
 };  
 
+// fetching API from server
 function currentWeather() {
     var city = cityInput.value;
     console.log(city);
     var queryURL= "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=d23b9ff5efa45588d81ffe68aaf47963";
     fetch(queryURL)
         .then(response => response.json())
-        .then(data => console.log(data));
+        .then(data =>{
+            console.log(data)
+            forecastWeather(data)
+        } );
+}
+
+function forecastWeather(obj) {
+    var oneCallUri ="https://api.openweathermap.org/data/2.5/onecall?lat=" + obj.coord.lat + "&lon=" + obj.coord.lon + "&appid=d23b9ff5efa45588d81ffe68aaf47963&units=imperial"
+    fetch(oneCallUri)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+        })
 }
 
 fetchButton.addEventListener('click', currentWeather);
