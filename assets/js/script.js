e0755052548b96a2f964e70beffeea4f820cbf84
Tommy Lee <t.k.hobbes43@gmail.com>
2022-05-14 22:28:04 -0500

    got colors showing now based on the UV index, but searches still not showing up in local storage, and still need to show 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,11 +43,25 @@ function currentWeather(city) {
             let uvIndex = uviResponse.current.uvi;
             let uvIndexPEl = $(`
                 <p>UV Index:
-                    <span id="unIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
+                    <span id="uvIndexColor" class="px-2 py-2 rounded">${uvIndex}</span>
                 <p>
             `);
 
             $("#displayCurrentWeather").append(uvIndexPEl);
+
+            // fiveDayForecast(lat, lon);
+
+            if (uvIndex >= 0 && uvIndex <= 2) {
+                $("#uvIndexColor").css("background-color", "#3EA72D").css("color", "white");
+            } else if (uvIndex >= 3 && uvIndex <= 5) {
+                $("#uvIndexColor").css("background-color", "#FFF300");
+            } else if (uvIndex >= 6 && uvIndex <= 7) {
+                $("#uvIndexColor").css("background-color", "#F18B00");
+            } else if (uvIndex >= 8 && uvIndex <= 10) {
+                $("#uvIndexColor").css("background-color", "#E53210").css("color", "white");
+            } else {
+                $("#uvIndexColor").css("background-color", "#B567A4").css("color", "white"); 
+            };  
         })
 
     });
